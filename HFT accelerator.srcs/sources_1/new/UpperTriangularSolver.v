9174f48d70a477881e0b75488ab40ce2bffbe246
SidGP <siddhigprakash@gmail.com>
2024-10-14 23:20:28 +0800

    Finished Upper Triangular Solver

--- HFT accelerator.srcs/sources_1/new/UpperTriangularSolver.v
+++ HFT accelerator.srcs/sources_1/new/UpperTriangularSolver.v
@@ -27,6 +27,15 @@ module UpperTriangularSolver #(Elements=4, ElementBits =8)(
     );
     
     wire [((Elements*Elements) * ElementBits) -1:0] TransposeQ; 
-    Transpose (Q, TransposeQ); 
+    wire [((Elements*Elements) * ElementBits) -1:0] TransposeV; 
+    
+//    Transpose (Q,TransposeQ); 
+    
+    ColumnSolver inst0(R, Q[127:96], TransposeV[127:96]); 
+    ColumnSolver inst1(R, Q[95:64], TransposeV[95:96]); 
+    ColumnSolver inst2(R, Q[63:32], TransposeV[63:32]); 
+    ColumnSolver inst3(R, Q[31:0], TransposeV[31:0]); 
+    
+    Transpose (V, TransposeV); 
 
 endmodule
